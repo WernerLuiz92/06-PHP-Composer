becf8844dff85f1351c944e8b061b9a0eb18dfd5
Werner Luiz Gottschalt <werner@lumiun.com>
2021-02-26 12:02:45 -0300

    Fazendo a busca no DOM

---
+++ buscadorDeCursos/buscador.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client();
+
+$response = $client->request('get', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $response->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+  echo $curso->textContent . '<br>';
+}
